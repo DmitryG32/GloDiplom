d200dfa1e3fc99c98a3a4252e63fc9361be246fd
Dmitry Gorbach <89874530087@mail.ru>
2022-06-08 18:26:52 +0200

    add btnScroll

--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -1,5 +1,8 @@
 const smoothScroll = () => {
   const menuBtnAll = document.querySelectorAll(".top-menu a");
+  const btnScrollTop = document.querySelector(".up");
+
+  btnScrollTop.style.display = "none";
 
   menuBtnAll.forEach((elem) => {
     elem.addEventListener("click", (event) => {
@@ -15,6 +18,26 @@ const smoothScroll = () => {
         });
       }
     });
+  });
+
+  window.addEventListener("scroll", () => {
+    let scrennHeight = document.documentElement.clientHeight;
+    let coords = window.pageYOffset;
+
+    console.log("clientHeight", scrennHeight);
+    console.log("pageYOffset", coords);
+
+    if (scrennHeight > coords) {
+      btnScrollTop.style.display = "none";
+    } else if (scrennHeight < coords) {
+      btnScrollTop.style.display = "block";
+    }
+  });
+
+  btnScrollTop.addEventListener("click", () => {
+    const html = document.querySelector("html");
+    html.style.scrollBehavior = "smooth";
+    document.documentElement.scrollTop = 0;
   });
 };
 
