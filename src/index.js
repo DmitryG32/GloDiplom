24f8814c4dd71f00f9b4807e5ca2fd3e3188fb35
Dmitry Gorbach <89874530087@mail.ru>
2022-06-10 22:30:22 +0200

    add sendForm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import smoothScroll from "./modules/smoothScroll";
 import modal from "./modules/modal";
 import accordeon from "./modules/accordeon";
+import sendForm from "./modules/sendForm";
 
 smoothScroll();
 modal();
 accordeon();
+sendForm();
--- src/modules/accordeon.js
+++ src/modules/accordeon.js
@@ -5,7 +5,7 @@ const accordeon = () => {
 
   accorText.forEach((block, index) => {
     if (accorItems[index] != accorItems[0]) {
-      block.style.transition = "all .3s";
+      block.style.transition = "all .5s";
       block.style.overflow = "hidden";
       block.style.maxHeight = "0px";
       block.style.padding = "0 20px";
@@ -21,7 +21,7 @@ const accordeon = () => {
         if (tab == descr && accorItems[index].classList.contains("active")) {
           accorItems[index].classList.remove("active");
           accorItems[index].style.paddingBottom = "0px";
-          tab.style.transition = "all .3s";
+          tab.style.transition = "all .5s";
           tab.style.overflow = "hidden";
           tab.style.maxHeight = "0px";
           tab.style.padding = "0 20px";
@@ -34,7 +34,7 @@ const accordeon = () => {
         } else {
           accorItems[index].classList.remove("active");
           accorItems[index].style.paddingBottom = "0px";
-          tab.style.transition = "all .3s";
+          tab.style.transition = "all .5s";
           tab.style.overflow = "hidden";
           tab.style.maxHeight = "0px";
           tab.style.padding = "0 20px";
