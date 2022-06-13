f5fb31f5d424f0668eae209f0cd510817e4615e8
Dmitry Gorbach <89874530087@mail.ru>
2022-06-14 00:52:56 +0200

    clear files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import smoothScroll from "./modules/smoothScroll";
 import modal from "./modules/modal";
 import accordeon from "./modules/accordeon";
-import sendForm from "./modules/sendForm";
 import slider from "./modules/slider";
 import sliderServices from "./modules/slider-services";
 import sendForm1 from "./modules/sendForm1";
@@ -9,7 +8,6 @@ import sendForm1 from "./modules/sendForm1";
 smoothScroll();
 modal();
 accordeon();
-//sendForm();
 slider();
 sliderServices();
 sendForm1();
--- src/modules/sendForm1.js
+++ src/modules/sendForm1.js
@@ -11,51 +11,6 @@ const sendForm1 = () => {
   let nameVal;
   let telVal;
 
-  /* function maskPhone(selector, masked = "+7 (__) --") {
-    const elems = document.querySelectorAll(selector);
-
-    function mask(event) {
-      const keyCode = event.keyCode;
-      const template = masked,
-        def = template.replace(/\D/g, ""),
-        val = this.value.replace(/\D/g, "");
-      console.log(template);
-      let i = 0,
-        newValue = template.replace(/[_\d]/g, function (a) {
-          return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-        });
-      i = newValue.indexOf("_");
-      if (i != -1) {
-        newValue = newValue.slice(0, i);
-      }
-      let reg = template
-        .substr(0, this.value.length)
-        .replace(/_+/g, function (a) {
-          return "\\d{1," + a.length + "}";
-        })
-        .replace(/[+()]/g, "\\$&");
-      reg = new RegExp("^" + reg + "$");
-      if (
-        !reg.test(this.value) ||
-        this.value.length < 5 ||
-        (keyCode > 47 && keyCode < 58)
-      ) {
-        this.value = newValue;
-      }
-      if (event.type == "blur" && this.value.length < 5) {
-        this.value = "";
-      }
-    }
-
-    for (const elem of elems) {
-      elem.addEventListener("input", mask);
-      elem.addEventListener("focus", mask);
-      elem.addEventListener("blur", mask);
-    }
-  }
-
-  maskPhone("input[name='tel']"); */
-
   window.addEventListener("DOMContentLoaded", function () {
     [].forEach.call(document.querySelectorAll(".tel"), function (input) {
       var keyCode;
@@ -95,7 +50,6 @@ const sendForm1 = () => {
       input.addEventListener("focus", mask, false);
       input.addEventListener("blur", mask, false);
       input.addEventListener("keydown", mask, false);
-      console.log("fff");
     });
   });
 
@@ -111,7 +65,6 @@ const sendForm1 = () => {
   };
 
   const validateTel = (tel) => {
-    /*  let reg = /^([\+]{0,1}[\d]{6,16})$/; */
     let reg = /[\d\+\-\(\)\ ]{16,}/;
     return reg.test(String(tel));
   };
@@ -173,7 +126,6 @@ const sendForm1 = () => {
       }
 
       if (!validateTel(telVal)) {
-        console.log("ошибка");
         inputTel.classList.add("error");
       }
 
